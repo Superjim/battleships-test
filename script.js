d626da0ce6ee81388a0bd8a69bf40b247d917d62
Jim Jenkinson <jamesjenkinson10@gmail.com>
2022-05-09 11:19:35 +0100

    Added ship array, function to get random position, function to check ship fits

--- script.js
+++ script.js
@@ -26,14 +26,39 @@ function renderGrid(size) {
   gridLocation.appendChild(table);
 }
 
-function checkHit() {
-  if (this.containsShip) {
-    setHit(this);
-  } else {
-    setMiss(this);
-  }
-}
+//Hasbro rules
 
+// No. 	Class of ship 	Size
+// 1 	Carrier 	5
+// 2 	Battleship 	4
+// 3 	Destroyer 	3
+// 4 	Submarine 	3
+// 5 	Patrol Boat 	2
+
+const ships = [
+  {
+    name: "Carrier",
+    length: 5,
+  },
+  {
+    name: "Battleship",
+    length: 4,
+  },
+  {
+    name: "Destroyer",
+    length: 3,
+  },
+  {
+    name: "Submarine",
+    length: 3,
+  },
+  {
+    name: "Patrol Boat",
+    length: 2,
+  },
+];
+
+//gets the coords from the inner text of the table cell ("x,y") and calls checkGrid function
 function getCoords() {
   splitCoords(this.innerText);
   if (checkGrid(splitCoords(this.innerText), enemyGrid) === true) {
@@ -42,26 +67,26 @@ function getCoords() {
   setMiss(this.innerText);
 }
 
+//splits the inner text of the cell into an array [x][y] to be sent "server" side
 function splitCoords(coords) {
   let split = coords.split(",");
   return split;
 }
 
+//sets the class of the cell stored in the table to miss (white)
 function setMiss(coords) {
   document.getElementById(coords).classList.replace("empty", "miss");
 }
 
+//sets the class of the cell stored in the table to hit (red)
 function setHit(coords) {
   document.getElementById(coords).classList.replace("empty", "hit");
 }
 
+//for testing
 renderGrid(10);
-
 let heroGrid = createBoard(10);
 let enemyGrid = createBoard(10);
-
-let myShips = 3;
-let enemyShips = 3;
 
 //creates an array of arrays to be used to store information on ship locations, hits and misses
 function createBoard(size) {
@@ -103,6 +128,7 @@ function createHeaders(size) {
 }
 
 //place a ship on the grid by changing the character stored in the array
+//eg "-" becomes E in the array
 function placeShip(x, y, size, orientation, char, grid) {
   if (orientation === "x") {
     for (i = 0; i < size; i++) {
@@ -111,6 +137,32 @@ function placeShip(x, y, size, orientation, char, grid) {
   } else {
     for (i = 0; i < size; i++) {
       grid[y + i][x] = char;
+    }
+  }
+}
+
+//gets a random position on the board via math random, returns it
+function getRandomPosition(size) {
+  let x = Math.floor(Math.random() * size);
+  let y = Math.floor(Math.random() * size);
+  const coords = [x, y];
+  return coords;
+}
+
+//if ship will go out of bounds, return false
+//if x cord is 8, and ship length is 5, and board size is 10, return false
+function checkShipFits(coords, orientation, ship, boardsize) {
+  if (orientation === "x") {
+    if (coords[0] + ship.length > boardsize) {
+      return false;
+    } else {
+      return true;
+    }
+  } else {
+    if (coords[1] + ship.length > boardsize) {
+      return false;
+    } else {
+      return true;
     }
   }
 }

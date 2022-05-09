9fbac60c61204e570d8102e6d5a4ef8e49039d5b
Jim Jenkinson <jamesjenkinson10@gmail.com>
2022-05-09 08:06:44 +0100

    Game board created

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
-class gameBoard {
-  constructor(size = 10) {
-    this.size = size;
-    const grid = new Array(size * size);
-    grid.fill("0");
-    return grid;
+let gridLocation = document.getElementById("gridContainer");
+
+function renderGrid(size) {
+  const body = document.body,
+    table = document.createElement("table");
+  table.id = "table";
+  table.style.width = (size * 50).toString() + "px";
+  table.style.height = (size * 50).toString() + "px";
+  table.style.border = "1px solid black";
+
+  for (let i = 1; i < size + 1; i++) {
+    const tr = table.insertRow();
+    tr.id = "row" + i;
+    for (let j = 1; j < size + 1; j++) {
+      const td = tr.insertCell();
+      td.id = "cell" + j;
+      td.addEventListener("click", setMiss);
+      td.classList.add("empty");
+      // td.appendChild(document.createElement("div"));
+      // td.innerText = i + "," + j;
+      td.style.border = "1px solid black";
+    }
   }
+  gridLocation.appendChild(table);
 }
 
-for object in gameBoard {
-    square = document.createElement("div");
-    square.innerText = "x";
-
+function getCoords() {
+  alert(this.innerText);
 }
+
+function setHit() {
+  this.classList.remove("empty");
+  this.classList.add("hit");
+}
+
+function setMiss() {
+  this.classList.remove("empty");
+  this.classList.add("miss");
+}
+
+renderGrid(10);
